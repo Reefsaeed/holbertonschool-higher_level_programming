f52ebe5a2f73fb5ba3cf65260a9f86d27f635272
Reef Saeed <reefsaeed@hotmail.com>
2025-12-06 00:14:59 +0000

    Task 1: Fix style issues in matrix division

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Matrix division module."""
+
+
+def matrix_divided(matrix, div):
+    """Divide matrix by div."""
+    err_msg = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not isinstance(matrix, list):
+        raise TypeError(err_msg)
+
+    if len(matrix) == 0:
+        raise TypeError(err_msg)
+
+    row_len = None
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(err_msg)
+
+        if len(row) == 0:
+            raise TypeError(err_msg)
+
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError(err_msg)
+
+        if row_len is None:
+            row_len = len(row)
+        elif len(row) != row_len:
+            msg = "Each row of the matrix must have the same size"
+            raise TypeError(msg)
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(round(num / div, 2))
+        new.append(new_row)
+
+    return new
