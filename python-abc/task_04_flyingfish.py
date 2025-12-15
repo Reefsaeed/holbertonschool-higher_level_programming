d19584f5d865f9709b593bf964e05e0d9464828d
Reef Saeed <reefsaeed@hotmail.com>
2025-12-15 08:01:09 +0000

    Implement FlyingFish with multiple inheritance from Fish and Bird

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+The Enigmatic FlyingFish - Exploring Multiple Inheritance
+"""
+
+
+class Fish:
+    """
+    Fish class representing aquatic animals
+    """
+    
+    def swim(self):
+        """
+        Print swimming message
+        """
+        print("The fish is swimming")
+    
+    def habitat(self):
+        """
+        Print fish habitat
+        """
+        print("The fish lives in water")
+
+
+class Bird:
+    """
+    Bird class representing aerial animals
+    """
+    
+    def fly(self):
+        """
+        Print flying message
+        """
+        print("The bird is flying")
+    
+    def habitat(self):
+        """
+        Print bird habitat
+        """
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """
+    FlyingFish class that inherits from both Fish and Bird
+    Demonstrates multiple inheritance
+    """
+    
+    def fly(self):
+        """
+        Override Bird's fly method for flying fish
+        """
+        print("The flying fish is soaring!")
+    
+    def swim(self):
+        """
+        Override Fish's swim method for flying fish
+        """
+        print("The flying fish is swimming!")
+    
+    def habitat(self):
+        """
+        Override habitat method for flying fish
+        """
+        print("The flying fish lives both in water and the sky!")
