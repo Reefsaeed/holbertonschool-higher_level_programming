3c51888e9cad4af511ff9992ae0417420506a053
“reefqhtani” <reefsaeed@hotmail.com>
2026-01-15 16:06:54 +0000

    Add task 1: Filter states starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute SQL query to select states starting with 'N'
+    # Using BINARY to make the comparison case-sensitive
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cur.execute(query)
+
+    # Fetch all rows
+    rows = cur.fetchall()
+
+    # Print each row
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cur.close()
+    db.close()
